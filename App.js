7ae49b19e6b51b90a423a37645c9a6e49a8f74d2
yve-n <nguetcheyvecarelle@gmail.com>
2022-02-08 16:59:17 +0100

    ajout de la scroll view

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { View, Text, TextInput, Image, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Image, StyleSheet, ScrollView } from 'react-native';
 
 const Logo = require("./assets/images/m2i-logo.png");
 const imgUrl ="https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/256x256/4e12aef886065a104d78b6206528b6f4";
 export default function App() {
   const [state, setState] = useState("");
   return (
-    <View style={styles.container}>
-      <Image 
-      // source={Logo}
-      source={{uri: imgUrl}}
-      style={styles.imgLogo}
-      />
-      <Text style={styles.text}>2i-Tech-Paris-2</Text>
-      <TextInput placeholder='Entrer votre texte' placeholderTextColor='#fff'
-      value={state}
-      onChangeText={value => setState(value)}
-      style={styles.textInput}/>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image 
+        // source={Logo}
+        source={{uri: imgUrl}}
+        style={styles.imgLogo}
+        />
+        <Text style={styles.text}>2i-Tech-Paris-2</Text>
+        <TextInput placeholder='Entrer votre texte' placeholderTextColor='#fff'
+        value={state}
+        onChangeText={value => setState(value)}
+        style={styles.textInput}/>
+      </View>
+    </ScrollView>
+
   );
 }
 

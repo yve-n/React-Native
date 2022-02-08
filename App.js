cfcfcf9e19ab26c68842447ef2bb1fe322614707
yve-n <nguetcheyvecarelle@gmail.com>
2022-02-08 16:23:22 +0100

    initialisation du projet et styles

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color: 'white',fontWeight:'bold'}}>2i-Tech-Paris-2</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  container:{
+    flex:2,
+    backgroundColor: 'black',
+    borderColor:'red',
+    borderWidth:2,
     justifyContent: 'center',
+    alignItems:'center',
   },
-});
+  // text:{
+  //   color: 'white',
+  //   fontWeight:'bold',
+
+  // }
+})
+
+
